da70b41a51750db952c0edf9bb8f86d9e09fd7c9
Egzon Rukovci <egzonrukovci@gmail.com>
2025-07-24 21:01:40 +0200

    Add .gitattributes to normalize line endings

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import './Index.css'
+import { Sidebar } from './components/sidebar'
+
+function App() {
+
+  return (
+    <>
+      <Sidebar/>
+     <h1 className='text-red-500'>Hello World2</h1>
+    </>
+  )
+}
+
+export default App
